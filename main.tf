558d8ff4d567a6ec1f8e2082ac321639d0e1725b
AVMUser-52833080@LODSPRODMCA.onmicrosoft.com <AVMUser-52833080@LODSPRODMCA.onmicrosoft.com>
2025-07-09 07:38:37 -0700

    created resource group

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+  resource_provider_registrations = "core"
+  subscription_id = var.subscription_id
+}
+
+
+resource "azurerm_resource_group" "contoso_rg" {
+  name     = "contoso_rg"
+  location = "UK South"
+}
---
+++ sensitive.variables.tf
@@ -0,0 +1,4 @@
+variable "subscription_id" {
+  description = ""
+  default = "c22f1727-ebbf-47bf-a66d-65dc146f1396"
+}
