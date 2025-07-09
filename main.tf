44a4d8b65c7d42ec4dda53d205c90b4d54c8a706
AVMUser-52833080@LODSPRODMCA.onmicrosoft.com <AVMUser-52833080@LODSPRODMCA.onmicrosoft.com>
2025-07-09 08:07:39 -0700

    added locals

--- main.tf
+++ main.tf
@@ -8,18 +8,27 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-
-  }
+  features {}
   resource_provider_registrations = "core"
   subscription_id = var.subscription_id
 }
 
-
-resource "azurerm_resource_group" "contoso_rg" {
-  name     = "contoso_rg"
-  location = "UK South"
+locals {
+  prefix = "contoso"
+  region = "North Europe"
   tags = {
     cost_center = "contoso research"
   }
+}
+
+resource "azurerm_resource_group" "contoso_rg" {
+  name     = "${local.prefix}_rg"
+  location = local.region
+  tags     = local.tags
+}
+
+resource "azurerm_resource_group" "contoso_dev_rg" {
+  name     = "${local.prefix}_dev_rg"
+  location = local.region
+  tags     = local.tags
 }
