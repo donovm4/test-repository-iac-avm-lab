54cc697da599db854748c7a6aa640ee8e92fa559
AVMUser-52833080@LODSPRODMCA.onmicrosoft.com <AVMUser-52833080@LODSPRODMCA.onmicrosoft.com>
2025-07-09 08:19:55 -0700

    added variables.tf

--- main.tf
+++ main.tf
@@ -10,26 +10,7 @@ terraform {
 provider "azurerm" {
   features {}
   resource_provider_registrations = "core"
-  subscription_id = var.subscription_id
-}
-
-# No default means that we will be prompted for it? or supply with .tfvars? or supply with ENV variable input?
-variable "prefix" {}
-
-variable "region" {
-  type        = string
-  default     = "UK South"
-  description = "The Azure region to deploy resources"
-  validation {
-    condition     = contains(["UK South", "UK West", "North Europe", "West Europe", "East US", "West US"], var.region)
-    error_message = "Invalid region"
-  }
-}
-
-# No default means that we will be prompted for it? or supply with .tfvars? or supply with ENV variable input?
-variable "tags" {
-  type        = map(any)
-  description = "A map of tags"
+  subscription_id                 = var.subscription_id
 }
 
 resource "azurerm_resource_group" "contoso_rg" {
