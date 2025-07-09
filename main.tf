431391cb9e7858d59317fd79196f6481916094d7
AVMUser-52833080@LODSPRODMCA.onmicrosoft.com <AVMUser-52833080@LODSPRODMCA.onmicrosoft.com>
2025-07-09 08:13:55 -0700

    added .tfvars

--- main.tf
+++ main.tf
@@ -13,22 +13,33 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-locals {
-  prefix = "contoso"
-  region = "North Europe"
-  tags = {
-    cost_center = "contoso research"
+# No default means that we will be prompted for it? or supply with .tfvars? or supply with ENV variable input?
+variable "prefix" {}
+
+variable "region" {
+  type        = string
+  default     = "UK South"
+  description = "The Azure region to deploy resources"
+  validation {
+    condition     = contains(["UK South", "UK West", "North Europe", "West Europe", "East US", "West US"], var.region)
+    error_message = "Invalid region"
   }
 }
 
+# No default means that we will be prompted for it? or supply with .tfvars? or supply with ENV variable input?
+variable "tags" {
+  type        = map(any)
+  description = "A map of tags"
+}
+
 resource "azurerm_resource_group" "contoso_rg" {
-  name     = "${local.prefix}_rg"
-  location = local.region
-  tags     = local.tags
+  name     = "${var.prefix}_rg"
+  location = var.region
+  tags     = var.tags
 }
 
 resource "azurerm_resource_group" "contoso_dev_rg" {
-  name     = "${local.prefix}_dev_rg"
-  location = local.region
-  tags     = local.tags
+  name     = "${var.prefix}_dev_rg"
+  location = var.region
+  tags     = var.tags
 }
