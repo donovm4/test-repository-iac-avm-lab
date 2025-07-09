02410bc65a3f4549de0bfadf27abfa4fbb99f7bd
AVMUser-52833080@LODSPRODMCA.onmicrosoft.com <AVMUser-52833080@LODSPRODMCA.onmicrosoft.com>
2025-07-09 07:54:20 -0700

    updating .gitignore, removing sensitive, adding tags

--- main.tf
+++ main.tf
@@ -19,4 +19,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "contoso_rg" {
   name     = "contoso_rg"
   location = "UK South"
+  tags = {
+    cost_center = "contoso research"
+  }
 }
--- sensitive.variables.tf
+++
@@ -1,4 +0,0 @@
-variable "subscription_id" {
-  description = ""
-  default = "c22f1727-ebbf-47bf-a66d-65dc146f1396"
-}
