135b08da93f6f291197b833baf839a2a7b0b0684
AVMUser-52833080@LODSPRODMCA.onmicrosoft.com <AVMUser-52833080@LODSPRODMCA.onmicrosoft.com>
2025-07-10 07:31:47 -0700

    added virtual networks and subnets

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "resource_groups" {
-  value = { for key, value in azurerm_resource_group.demo : key => value.id }
+  value       = { for key, value in azurerm_resource_group.demo : key => value.id }
   description = "resource group ids"
 }
